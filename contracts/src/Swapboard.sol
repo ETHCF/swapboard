808d144e3de1dc9ec02401562f83b154edd89b4b
lbeder <leonid@lbeder.com>
2026-08-06 19:30:26 +0100

    Update Solidity to 0.8.36

--- contracts/src/Swapboard.sol
+++ contracts/src/Swapboard.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.33;
+pragma solidity 0.8.36;
 
 import {ReentrancyGuardTransient} from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
