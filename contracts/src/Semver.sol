adec53d2cfd4fe72395d3418cdcd59271615e378
lbeder <leonid@lbeder.com>
2026-08-08 20:50:31 +0100

    Add Semver contracts

---
+++ contracts/src/Semver.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.36;
+
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {ISemver} from "./interfaces/ISemver.sol";
+
+/// @title Semver
+/// @notice Simple base contract for managing semantic version numbers
+contract Semver is ISemver {
+    /// @notice Contract major version number
+    uint256 private immutable _MAJOR;
+
+    /// @notice Contract minor version number
+    uint256 private immutable _MINOR;
+
+    /// @notice Contract patch version number
+    uint256 private immutable _PATCH;
+
+    /// @param major Major version number
+    /// @param minor Minor version number
+    /// @param patch Patch version number
+    constructor(
+        uint256 major,
+        uint256 minor,
+        uint256 patch
+    ) {
+        _MAJOR = major;
+        _MINOR = minor;
+        _PATCH = patch;
+    }
+
+    /// @inheritdoc ISemver
+    function version() external view returns (string memory) {
+        return string(
+            abi.encodePacked(Strings.toString(_MAJOR), ".", Strings.toString(_MINOR), ".", Strings.toString(_PATCH))
+        );
+    }
+}
