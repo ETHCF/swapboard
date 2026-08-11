5ff4dbaceacf9483fc9bca5da3efb5bb980ea1f3
lbeder <leonid@lbeder.com>
2026-08-11 20:26:12 +0100

    Fix lint

--- contracts/src/interfaces/ISemver.sol
+++ contracts/src/interfaces/ISemver.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 /// @title ISemver
+/// @author Zak Cole (numbergroup.xyz) for Ethereum Community Foundation
 /// @notice Interface for contracts that expose a semantic version string
 interface ISemver {
     /// @notice Returns the full semver contract version
